c782405e89fb32f2d54d74ee5b5deb9edc511246
Simran Suresh <simrans10496@gmail.com>
2022-04-15 23:40:41 +0200

    kickstarted with basic coordinate transformations

---
+++ constants.py
@@ -0,0 +1,78 @@
+# Class: Constants
+# Contents: Earth constants, Geodetic reference system constants
+# Functions: 
+# Author: Simran Suresh
+# Date: 15.04.2022
+
+class Constants:
+    def __init__(self):
+        # SRC: from Wiki
+
+        # mass of earth [kg]
+        self.M = 5.9722 * 10e24
+
+        # gravitational constant [m3/kg*s2] defined in Newton's law of gravitation
+        self.G = 6.674 * 10e-11
+
+        # acceleration due to gravity [m/s2]
+        self.g = 9.8067
+
+        # radius of the earth rounded [km]
+        self.R = 6371
+
+
+class Reference_Systems:
+    # https://de.wikipedia.org/wiki/World_Geodetic_System_1984
+
+    def __init__(self):
+
+        # semi major axis or equatorial radius [m]
+        self.a = 6378137
+
+        # geocentric gravitational constant = gravitational constant * earth mass [m3/s2]
+        self.GM = 3.986004418 * 10**14
+
+        # rotational speed of earth or angular velocity omega_e [rad/s]
+        self.omega_e = 7.292115 * 10**-5
+
+        # derived constants
+        # semi minor axis b = a(1-f) = 6356752 [m]
+        self.b = self.a * (1 - self.f)
+
+        # eccentricity e owing to the bent of the ellipse
+        # first eccentricity e2 = 1 - b2/a2 = 2f - f2
+        self.e2 = 1 - self.b**2 / self.a**2
+
+        # second eccentricity e′2 = a2/b2 - 1 = f(2-f)/(1-f)2
+        self.e2_ = self.a**2 / self.b**2 - 1
+
+
+class WGS84(Reference_Systems):
+    # World Geodetic System 1984 (WGS84) is the best fit global datum
+    # https://gssc.esa.int/navipedia/index.php/Reference_Frames_in_GNSS#GPS_reference_frame_WGS-84
+
+    def __init__(self):
+
+        # flattening of the earth
+        self.f = 1 / 298257223563
+
+        # super class constructor called here so that self.f is referenced 
+        super().__init__()
+
+
+class GRS80(Reference_Systems):
+    # Geodetic Reference System 1980 (GRS80) reference system similar to WGS84
+    # contains global reference ellipsoid and normal gravity model
+    # https://en.wikipedia.org/wiki/Geodetic_Reference_System_1980
+
+    def __init__(self):
+
+        # flattening
+        self.f = 1 / 298.257222100882711243
+
+        # dynamic form factor J2
+        self.j2 = 108263 * 10**-8
+
+        super().__init__()
+
+        
---
+++ coordinate_transformations.py
@@ -0,0 +1,119 @@
+# Class: CoordTf
+# Contents: Frequently used geodetic transformations
+# Functions: cart2ellip, ellip2cart, cart2sph, sph2cart
+# Author: Simran Suresh
+# Date: 15.04.2022
+
+import numpy as np
+from constants import WGS84, GRS80, Constants
+
+
+class CoordTf:
+    def __init__(self):
+        pass
+
+    def cart2ellip(self, x, y, z, ref=None):
+        """
+        SRC: https://gssc.esa.int/navipedia/index.php/Ellipsoidal_and_Cartesian_Coordinates_Conversion
+        transformation of cartesian to ellipsoidal or llh coordinates or ECEF coordinates
+        x: x coordinate/easting
+        y: y coordinate/northing
+        z: height 
+        ref: WGS84 or GRS80. Default will be WGS84
+        returns: x,y,z
+        """
+        # set reference system
+        ref = GRS80() if ref == "GRS80" else WGS84()
+
+        # ellipsoidal longitude
+        lon = np.arctan(y / x)
+
+        # ellipsoidal latitude, h computation needs iteration as h needs lat and lat needs h too
+        # init
+        p = np.sqrt(x**2 + y**2)
+        h0 = 0
+        lat0 = np.arctan(z / (1 - ref.e2)*p)
+        epsilon = 0.1e-3
+        iter = True
+
+        # iteration
+        while iter:
+            # radius of curvature changes wrt lat
+            N = ref.a / np.sqrt(1 - ref.e2*np.sin(lat0)**2)
+            h = p / np.cos(lat0) - N
+            lat = np.arctan((z / p) / (1 - ref.e2* N / (N+h)))
+
+            if abs(h-h0) < epsilon and Constants().R * abs(lat-lat0) < epsilon:
+                iter = False
+
+            h0 = h
+            lat0 = lat
+
+        return np.rad2deg(lat), np.rad2deg(lon), h
+
+
+    def ellip2cart(self, lat, lon, h, ref=None):
+        """
+        SRC: https://gssc.esa.int/navipedia/index.php/Ellipsoidal_and_Cartesian_Coordinates_Conversion
+        transformation of ellipsoidal or llh coordinates to cartesian 
+        lat: latitude 
+        lon: longitude
+        h: ellipsoidal height
+        ref: WGS84 or GRS80. Default will be WGS84
+        returns: lat, lon, h
+        """
+        # set reference system
+        ref = GRS80() if ref == "GRS80" else WGS84()
+
+        # np sin, cos accept in radian format only
+        lat, lon = np.deg2rad(lat), np.deg2rad(lon)
+
+        # radius of curvature
+        N = ref.a / (1 - ref.e2*np.sin(lat)**2)
+
+        # cartesian coordinates
+        x = (N + h) * np.cos(lat) * np.cos(lon)
+        y = (N + h) * np.cos(lat) * np.sin(lon)
+        z = ((1 - ref.e2)*N + h) * np.sin(lat)
+
+        return x, y, z
+
+
+    def cart2sph(self, x, y, z):
+        """
+        SRC: https://en.wikipedia.org/wiki/Spherical_coordinate_system
+        transformation of cartesian to spherical coordinates
+        x,y,z: Cartesian coordinates
+        returns: r, inclination, azimuth
+        """
+        # radius
+        r = np.sqrt(x**2 + y**2 + z**2)
+
+        # inclination theta
+        theta = np.arctan(np.sqrt(x**2 + y**2) / z)
+
+        # azimuth angle phi
+        phi = np.arctan(y / x)
+
+        return r, np.rad2deg(theta), np.rad2deg(phi)
+
+
+    def sph2cart(self, r, theta, phi):
+        """
+        SRC: https://en.wikipedia.org/wiki/Spherical_coordinate_system
+        transformation of spherical to cartesian coordinates
+        r, inclination, azimuth: spherical coordinates
+        returns: x, y, z
+        """
+        # np sin, cos accept in radian format only
+        theta, phi = np.deg2rad(theta), np.deg2rad(phi)
+
+        # cartesian coordinates
+        x = r * np.sin(theta) * np.cos(phi)
+        y = r * np.sin(theta) * np.sin(phi)
+        z = r * np.cos(theta)
+
+        return x, y, z
+
+
+# TODO: ECEF to ENU, ENU to ECEF
